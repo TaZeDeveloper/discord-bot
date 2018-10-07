1a9a9a7ebbfd2a4c9a0f2ab406a271a3c7c51a9b
TaZe_Pug <aidan123.hudson@aol.com>
2018-10-07 23:11:48 +0100

    Security Fix

--- src/main/java/com/discordbot/Updated_Discord_Bot/Config.java
+++ src/main/java/com/discordbot/Updated_Discord_Bot/Config.java
@@ -1,6 +1,6 @@
 package com.discordbot.Updated_Discord_Bot;
 
 public class Config {
-	public static final String token = "NDk4NTQxMDgwOTc2MDMxNzc2.DpvO-Q.IcceXxNH0_47bHh386taAyZ4a-4";
+	public static final String token = "TOKEN_HERE";
 	public static final String prefix = "/";
 }
