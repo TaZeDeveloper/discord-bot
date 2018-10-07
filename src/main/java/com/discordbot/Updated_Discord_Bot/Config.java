e2c2abdd9cee77c032704cbe7f98a45396a32865
TaZerServers <aidan123.hudson@aol.com>
2018-10-07 09:56:50 +0100

    Add Prime Files
    Added Prime Files & Classes including:
    Config, Utils

---
+++ src/main/java/com/discordbot/Updated_Discord_Bot/Config.java
@@ -0,0 +1,6 @@
+package com.discordbot.Updated_Discord_Bot;
+
+public class Config {
+	public static final String token = "NDkyOTQ2NTYzNTA1NzE3MjQ4.Dpa4UQ.uEyV1bunfgU9Rhs1Coj1W7nX2sA";
+	public static final String prefix = "/";
+}
