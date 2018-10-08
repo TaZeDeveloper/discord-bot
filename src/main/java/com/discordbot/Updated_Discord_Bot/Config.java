38e874ce8131bde27efb206e901cda7557668726
TaZe_Pug <aidan123.hudson@aol.com>
2018-10-08 08:32:42 +0100

    Update Config.java

--- src/main/java/com/discordbot/Updated_Discord_Bot/Config.java
+++ src/main/java/com/discordbot/Updated_Discord_Bot/Config.java
@@ -1,6 +1,6 @@
 package com.discordbot.Updated_Discord_Bot;
 
 public class Config {
-	public static final String token = "TOKEN_HERE";
+	public static final String token = "NDk4NTQxMDgwOTc2MDMxNzc2.DpyZNQ.rmjKgORHDkYUrwd3diLpT7ZGDI4";
 	public static final String prefix = "/";
 }
