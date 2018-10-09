49a1a117e7ae1939b4fa9e0a16c7ca77092da2fd
TaZe_Pug <aidan123.hudson@aol.com>
2018-10-09 20:36:38 +0100

    Edited Config
    Added a env

--- src/main/java/com/discordbot/Updated_Discord_Bot/Config.java
+++ src/main/java/com/discordbot/Updated_Discord_Bot/Config.java
@@ -1,6 +1,6 @@
 package com.discordbot.Updated_Discord_Bot;
 
 public class Config {
-	public static final String token = "NDk4NTQxMDgwOTc2MDMxNzc2.DpyZNQ.rmjKgORHDkYUrwd3diLpT7ZGDI4";
+	public static final String token = process.env.BOT_TOKEN;
 	public static final String prefix = "/";
 }
